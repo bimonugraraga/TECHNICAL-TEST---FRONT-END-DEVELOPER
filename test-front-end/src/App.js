0a2448af87bb7cdaa25f8e7bcb91d126ae4a8822
Erno <bimonugraraga@gmail.com>
2022-03-31 02:38:53 +0700

    penyelesaian test

--- test-front-end/src/App.js
+++ test-front-end/src/App.js
@@ -1,54 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
-import axios from 'axios'
-import {useEffect, useState} from 'react'
 import './styling/Home.css'
-
+import HomeScreen from './view/Home';
+import { BrowserRouter ,Routes, Route, Link } from "react-router-dom";
+import DetailScreen from './view/Detail';
+import AddScreen from './view/Add';
+import {useState, useEffect} from 'react'
+import { useNavigate } from 'react-router-dom';
 function App() {
-  let [data, setData] = useState([])
-  useEffect(() => {
-    axios.get('http://localhost:3000/data')
-      .then((resp) => {
-        let temp = resp.data.map((el) => {
-          let temp2 = []
-          for (const key in el.skills){
-            if (el.skills[key] === 'expert'){
-              temp2.push(key)
-            }
-          }
 
-
-          el.full_name = `${el.first_name} ${el.last_name}`
-          el.expert_skills = temp2
-          el.expert_skills2 = temp2.join(', ')
-          delete el.skills
-          return el
-        })
-     
-        setData(temp)
-      })
-      .catch((err) => {
-        console.log(err)
-      })
-  }, [])
-  console.log(JSON.stringify(data, null, 2))
   return (
-    
-    <div className='HomeApp'>
+    <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<HomeScreen/>}/>
+          <Route path='/:id' element={<DetailScreen />}/>
+          <Route path='/add' element={<AddScreen />}/>
+        </Routes>
 
-      {data?data.map((el, index) => {
-        // el.expert_skills = el.expert_skills.join(',')
-        return(
-          <div key={index}>
-
-            <p>{el.full_name} <br></br> {el.expert_skills2}</p>
-          </div>
-
-        )
-      }):<></>}
+      </BrowserRouter>
     </div>
-  
-  );
+  )
 }
 
 export default App;
