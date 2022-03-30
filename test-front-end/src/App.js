227ff1521546a6fc9259e40dad916e22124cef3e
Erno <bimonugraraga@gmail.com>
2022-03-30 22:08:50 +0700

    tampilan step 3

--- test-front-end/src/App.js
+++ test-front-end/src/App.js
@@ -2,13 +2,13 @@ import logo from './logo.svg';
 import './App.css';
 import axios from 'axios'
 import {useEffect, useState} from 'react'
+import './styling/Home.css'
 
 function App() {
   let [data, setData] = useState([])
   useEffect(() => {
     axios.get('http://localhost:3000/data')
       .then((resp) => {
-        console.log(resp.data)
         let temp = resp.data.map((el) => {
           let temp2 = []
           for (const key in el.skills){
@@ -17,31 +17,37 @@ function App() {
             }
           }
 
+
+          el.full_name = `${el.first_name} ${el.last_name}`
           el.expert_skills = temp2
+          el.expert_skills2 = temp2.join(', ')
           delete el.skills
           return el
         })
-
+     
         setData(temp)
       })
+      .catch((err) => {
+        console.log(err)
+      })
   }, [])
+  console.log(JSON.stringify(data, null, 2))
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    
+    <div className='HomeApp'>
+
+      {data?data.map((el, index) => {
+        // el.expert_skills = el.expert_skills.join(',')
+        return(
+          <div key={index}>
+
+            <p>{el.full_name} <br></br> {el.expert_skills2}</p>
+          </div>
+
+        )
+      }):<></>}
     </div>
+  
   );
 }
 
